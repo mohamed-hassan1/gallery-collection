c6b46e4c33755b0c163d682889c982d74d8cb983
mohamed-hassan1 <mohamedhassanmohamed1@gmail.com>
2025-02-16 21:21:56 +0200

    Show button function

--- masonry/js/main.js
+++ masonry/js/main.js
@@ -2,8 +2,9 @@ const galleryMasonry = (function() {
   let columnsCount = 4, gap = 15,
       colsHeight = new Array(columnsCount).fill(0),
       container = document.querySelector('.gallery-masonry'),
-      list = container.querySelector('.inner-content'),
-      allImgs = Array.from(container.querySelectorAll('.gallery-img'));
+      list = container.querySelector('.gallery-content .inner-content'),
+      allImgs = Array.from(container.querySelectorAll('.gallery-img')),
+      showBtn = container.querySelector('.show-btn');
 
   // Create Gallery Columns
   function UI_init() {
@@ -29,11 +30,55 @@ const galleryMasonry = (function() {
     container.classList.add('ready');
   }
 
+  // Gallery Container animation
+  showBtn.addEventListener('click', function() {
+    let content = container.querySelector('.gallery-content'),
+        contentHeight = content.children[0].offsetHeight,
+        speed = 5, smooth = 40, max = Number(window.getComputedStyle(content).minHeight.replace('px',''));
+
+    if (!content.classList.contains('active')) { // Slide Down
+      content.classList.add('active');
+      // Change Button text
+      this.textContent= 'Show Less';
+      let slideDown = setInterval(frame, speed),
+          counter = content.offsetHeight + smooth;
+
+      // Animation Function
+      function frame() {
+        content.style.height = counter + 'px';
+        counter += smooth;
+        if (counter >= contentHeight) { // Stop Animation
+          content.style.height = contentHeight + 'px';
+          clearInterval(slideDown);
+        }
+      }
+
+    } else { // Slide Up
+      content.classList.remove('active');
+      // Change Button text
+      this.textContent = 'Show More';
+      let slideDown = setInterval(frame, speed),
+      counter = contentHeight - smooth;
+
+      // Animation Function
+      function frame() {
+        content.style.height = counter + 'px';
+        counter -= smooth;
+        if (counter <= max) { // Stop Animation
+          content.style.height = max + 'px';
+          clearInterval(slideDown);
+        }
+      }
+    }
+  });
+
   UI_init();
   window.onload = function() {
     // Sort Images by height
     allImgs.sort((a,b) => b.height - a.height);
-    UI_gallery()
+    UI_gallery();
   }
+
+
 
 })();
